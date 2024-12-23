bd45eb434db7277bcd8f4156f123233a097dd767
Joey Fang <josudoey@gmail.com>
2024-12-23 21:39:02 +0800

    test: correct dbml case

--- dbml/column_test.go
+++ dbml/column_test.go
@@ -14,7 +14,7 @@ var _ = DescribeTable("Column", func(c *Column, expected string) {
 			Type:     "varchar(255)",
 			Settings: []ColumnSetting{Unique(), NotNull(), ColumnNote("to include unit number")},
 		},
-		`address varchar(255) [unique, not null, note: 'to include unit number']`),
+		`address "varchar(255)" [unique, not null, note: 'to include unit number']`),
 	Entry("Column id",
 		&Column{
 			Name:     "id",
